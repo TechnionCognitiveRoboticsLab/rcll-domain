1bd25028af4a235e1df75e018c50a99d716b2198
Erez Karpas <karpase@gmail.com>
2023-03-16 10:56:49 +0200

    generate all orders

--- 2016/game-parameters/gen-pddl-all.sh
+++ 2016/game-parameters/gen-pddl-all.sh
@@ -15,8 +15,8 @@ for i in $(seq -f "%03g" 1 100); do
 	## Disable RS for now as we only care about standing order
 	## Only single standing order as goal and input
 	# By number of robots
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=1 --no-rs --durations > ../pddl-production/problem-$i-r1-o1-durations.pddl
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=2 --no-rs --durations > ../pddl-production/problem-$i-r2-o1-durations.pddl
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=3 --no-rs --durations > ../pddl-production/problem-$i-r3-o1-durations.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=1 --no-rs --durations > ../pddl-production/problem-$i-r1-o1-durations.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=2 --no-rs --durations > ../pddl-production/problem-$i-r2-o1-durations.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=3 --no-rs --durations > ../pddl-production/problem-$i-r3-o1-durations.pddl
 
 done
