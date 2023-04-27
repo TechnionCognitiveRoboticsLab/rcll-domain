89d2e23d49eb500d04fb09e76b9090fa90f8cfd0
Erez Karpas <karpase@gmail.com>
2023-04-27 08:46:51 +0300

    added announce time in PDDL comment

--- 2016/game-parameters/gen-pddl-all.sh
+++ 2016/game-parameters/gen-pddl-all.sh
@@ -7,16 +7,16 @@ for i in $(seq -f "%03g" 1 100); do
 	## Disable RS for now as we only care about standing order
 	## Only single standing order as goal and input
 	# By number of robots
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=1 --no-rs > ../pddl-production/problem-$i-r1-o1.pddl
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=2 --no-rs > ../pddl-production/problem-$i-r2-o1.pddl
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=3 --no-rs > ../pddl-production/problem-$i-r3-o1.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=1 --no-rs > ../pddl-production/problem-$i-r1-all-orders.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=2 --no-rs > ../pddl-production/problem-$i-r2-all-orders.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --include-order=1 --num-robots=3 --no-rs > ../pddl-production/problem-$i-r3-all-orders.pddl
 
 	### Durative actions
 	## Disable RS for now as we only care about standing order
 	## Only single standing order as goal and input
 	# By number of robots
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=1 --no-rs --durations > ../pddl-production/problem-$i-r1-o1-durations.pddl
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=2 --no-rs --durations > ../pddl-production/problem-$i-r2-o1-durations.pddl
-	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=3 --no-rs --durations > ../pddl-production/problem-$i-r3-o1-durations.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=1 --no-rs --durations > ../pddl-production/problem-$i-r1-all-orders-durations.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=2 --no-rs --durations > ../pddl-production/problem-$i-r2-all-orders-durations.pddl
+	./to_pddl.py --template=problem.j2 -g game-$i.txt -n ../navgraph-data/navgraph-costs-$i.csv --problem-name-suffix=$i --num-robots=3 --no-rs --durations > ../pddl-production/problem-$i-r3-all-orders-durations.pddl
 
 done
